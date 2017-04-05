a0a89bedbd46ddfb73b6c9f39090096817172b83
Brian Pooe <brianp@zapper.local>
2017-04-05 15:43:22 +0200

    route configuration

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const api = require("./routes/api");
+const logger = require("morgan");
 
+const api = require("./routes/api");
+const app = express();
 const port = process.env.PORT | 3000;
 
-const app = express();
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("api", api);
+app.use("/api", api);
 
-app.get('*', (req, res) => {
-    res.send("404");
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.listen(port, "localhost", (req, res) => {
+app.listen(port, (req, res) => {
     console.log('Server running on localhost:/' + port);
 }); 
--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("contacts", (req, res) => {
+router.get("/contacts", (req, res) => {
     res.send("hit all contacts");
 });
 
-router.get("contacts/:id", (req, res) => {
-    res.send("hit specific contact")
+router.get("/contact/:id", (req, res) => {
+    res.send("hit specific contact" + req.params.id)
 });
 
-router.post("contact", (req, res) => {
+router.post("/contact", (req, res) => {
     res.send("hit adding contact");
 });
 
-router.put("contact/:id", (req, res) => {
+router.put("/contact/:id", (req, res) => {
     res.send("hit update contact");
 });
 
-router.delete("contact/:id", (req, res) => {
-
+router.delete("/contact/:id", (req, res) => {
+    res.send("hit specific contact" + req.params.id)
 });
+
+module.exports = router;
