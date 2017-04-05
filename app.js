6cd3ceb8d1905de389d35865712799764fa69ea3
Brian Pooe <brianp@zapper.local>
2017-04-05 13:48:59 +0200

    adding server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const port = process.env.PORT | 3000;
+
+const app = express();
+
+app.listen(port, localhost, (req, res) => {
+
+});
