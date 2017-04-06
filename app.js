8113e625b870fb9da24c34c887af56c9bfac800d
Brian Pooe <brianp@zapper.local>
2017-04-06 09:34:51 +0200

    adding connection to mlab

--- app.js
+++ app.js
@@ -2,6 +2,35 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+const config = require("./config/database");
+
+// Create the database connection 
+mongoose.connect(config.database);
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on("connected", () => {
+  console.log('Mongoose default connection open to ' + config.database);
+});
+
+// If the connection throws an error
+mongoose.connection.on("error", (err) => {
+  console.log("Mongoose default connection error: " + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose default connection disconnected");
+});
+
+// If the Node process ends, close the Mongoose connection 
+process.on("IGINT", function () {
+  mongoose.connection.close(function () {
+    console.log("Mongoose default connection disconnected through app termination");
+    process.exit(0);
+  });
+});
 
 const api = require("./routes/api");
 const app = express();
@@ -14,12 +43,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", api);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 app.listen(port, (req, res) => {
-    console.log('Server running on localhost:/' + port);
+  console.log('Server running on localhost:/' + port);
 }); 
