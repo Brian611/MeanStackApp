37d215379e02704f8c5c8403848e6dfd24ccdfbd
Brian Pooe <brianp@zapper.local>
2017-04-06 11:54:30 +0200

    implementing passport

---
+++ config/passport.js
@@ -0,0 +1,20 @@
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const config = require("./database");
+
+const opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
+opts.secretOrKey = config.secret;
+
+passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
+    User.findOne({ id: jwt_payload._doc._id }, function (err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            done(null, user);
+        } else {
+            done(null, false);
+        }
+    });
+}));
