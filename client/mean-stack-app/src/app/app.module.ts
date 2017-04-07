f2b297503ff22913039f9f178ea765e3b99796b3
Brian Pooe <brianp@zapper.local>
2017-04-07 10:24:02 +0200

    added Reactive forms

--- client/mean-stack-app/src/app/app.module.ts
+++ client/mean-stack-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -33,7 +33,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule, ReactiveFormsModule,
     HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
--- client/mean-stack-app/src/app/components/login/login.component.ts
+++ client/mean-stack-app/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      username: ['brian', [Validators.required, Validators.minLength(2)]],
+      password: ['', [Validators.required, Validators.minLength(5)]]
+    });
   }
 
+  login() {
+    console.log(this.loginForm);
+  }
 }
