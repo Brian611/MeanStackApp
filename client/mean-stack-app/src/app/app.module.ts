761c71d31bd19e7b169d9dc5f2277a8e9903d3e9
Brian Pooe <brianp@zapper.local>
2017-04-07 09:25:19 +0200

    added components needed for the routes

--- client/mean-stack-app/src/app/app.module.ts
+++ client/mean-stack-app/src/app/app.module.ts
@@ -2,14 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+
+const appRoutes : Routes = [
+{path: '', redirectTo: '/home', pathMatch: 'full'},
+
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    ProfileComponent,
+    ContactsComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
