a82d485c2419e949fc2b7c859be2809f7ded700e
Brian Pooe <brianp@zapper.local>
2017-04-07 08:58:02 +0200

    navbar component

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ const user = require("./routes/user");
 const app = express();
 const port = process.env.PORT | 3000;
 
+app.use(express.static(path.resolve(__dirname, "public")));
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -50,10 +51,8 @@ app.use("/api", api);
 app.use("/api", user);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use((req, res) => {
+  res.render('index');
 });
 
 app.listen(port, (req, res) => {
